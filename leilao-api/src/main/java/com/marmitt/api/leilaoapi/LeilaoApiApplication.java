c726a3cec76e33f7a8b8fe710d6e0fb16320e607
mmarmit <mmarmit@ad.esi.adp.com>
2020-08-13 17:43:33 -0300

    removing module

---
+++ leilao-api/src/main/java/com/marmitt/api/leilaoapi/LeilaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.marmitt.api.leilaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeilaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeilaoApiApplication.class, args);
+	}
+
+}
